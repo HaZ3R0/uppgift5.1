911f2467a2dc7e8c050e9de358f67cea17f157f2
HaZ3R0 <megamackan@outlook.com>
2023-11-22 09:58:09 +0100

    Add project files.

---
+++ uppgift5.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace uppgift5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("du ska få skriva in 5 namn");
+            string[] namn = new string[5];
+            for (int i = 0; i < namn.Length; i++)
+            {
+                Console.WriteLine("Skriv in ett namn");
+                namn[i] = Console.ReadLine();
+            }
+            Console.WriteLine("Namnen du skrev var");
+
+            foreach (string i in namn)
+            {
+                Console.WriteLine(i);
+            }
+            
+        }
+    }
+}
